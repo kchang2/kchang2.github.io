811a0ea0b86b650da845763ba004234d53438869
Chang <Kai-TangC@ladodgers.com>
2017-11-24 02:15:29 -0800

    Updated navigation, aligned social media icons, beautify layout

--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+https://www.w3schools.com/howto/howto_js_tabs.asp
