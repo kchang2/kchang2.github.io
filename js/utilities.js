23ed60526d47c62489983770b6a8d8132f42957c
Chang <Kai-TangC@ladodgers.com>
2017-12-17 22:39:10 -0800

    Added working navigation vs. js

--- js/utilities.js
+++ js/utilities.js
@@ -2,16 +2,28 @@
 
 var selector = '.nav-tabs a';
 
+/* Creating navigation via HTML DOM innterHTML properties */
+document.getElementById("navMenuIndex").innerHTML =
+	`
+	<!-- Use https://w3schools.com/howto/howto_js_topnav_responsive.asp -->
+	<div class="nav-tabs" id="nav">
+		<a class="active" href="index.html">Home</a>
+		<a href="./pages/motto.html">Motto</a>
+		<a href="#">Projects / Research</a>
+		<a href="./pages/academics.html">Academics</a>
+		<a href="./pages/contact.html">Contact</a>
+	</div>`
+document.getElementById("navMenu").innerHTML =
+	`<div class="nav-tabs" id="nav">
+	<a href="../index.html">Home</a>
+	<a href="motto.html">Motto</a>
+	<a href="#">Projects / Research</a>
+	<a href="academics.html">Academics</a>
+	<a class="active" href="contact.html">Contact</a>
+	</div>`
+
+
 $(selector).click(function(){
 	$(selector).removeClass('active');
 	$(this).addClass('active');
 });
-
-// $.get("./pages/navigation.html", function(data) {
-// 	$("#nav-placeholder").replaceWwith(data);
-// });
-
-
-// $(function(){
-// 	$('#nav-placeholder').load('../pages/navigation.html');
-// });
