7c3bc14e50b347a612ea8dfa4145836e66ebefc8
Chang <Kai-TangC@ladodgers.com>
2018-01-15 14:00:05 -0800

    Fixed and finalized first version of website

--- js/utilities.js
+++ js/utilities.js
@@ -2,28 +2,46 @@
 
 var selector = '.nav-tabs a';
 
-/* Creating navigation via HTML DOM innterHTML properties */
-document.getElementById("navMenuIndex").innerHTML =
+try {
+	/* Creating navigation via HTML DOM innterHTML properties */
+	document.getElementById("navMenuIndex").innerHTML =
 	`
 	<!-- Use https://w3schools.com/howto/howto_js_topnav_responsive.asp -->
 	<div class="nav-tabs" id="nav">
 		<a class="active" href="index.html">Home</a>
 		<a href="./pages/motto.html">Motto</a>
-		<a href="#">Projects / Research</a>
-		<a href="./pages/academics.html">Academics</a>
+		<a href="./pages/research.html">Projects / Research</a>
 		<a href="./pages/contact.html">Contact</a>
-	</div>`
-document.getElementById("navMenu").innerHTML =
-	`<div class="nav-tabs" id="nav">
-	<a href="../index.html">Home</a>
-	<a href="motto.html">Motto</a>
-	<a href="#">Projects / Research</a>
-	<a href="academics.html">Academics</a>
-	<a class="active" href="contact.html">Contact</a>
-	</div>`
+	</div>`;
+}
+catch(err) {
+	/* Create navigation for non-index pages*/
+	document.getElementById("navMenu").innerHTML =
+	`
+	<div class="nav-tabs" id="nav">
+		<a href="../index.html">Home</a>
+		<a id="Motto" href="motto.html">Motto</a>
+		<a id="Research" href="research.html">Projects / Research</a>
+		<a id="Contact" href="contact.html">Contact</a>
+	</div>
+	`;
+
+	var text = document.getElementsByTagName("title")[0]['innerHTML'];
+	document.getElementById(text).className += 'active';
+}
+
+/* Setting up links at footer */
+document.getElementsByClassName("footer")[0]['innerHTML'] = 
+	`
+	<a class="soc soc-initial-color soc-linkedin soc-icon-last" href="#"></a>
+	<a class="soc soc-initial-color soc-github soc-icon-last" href="#"></a>
+	<a class="icon-ext soc-initial-color icon-ladodgers soc-icon-last" href="#"></a>
+	<a class="icon-ext2 soc-initial-color icon-caltech soc-icon-last" href="#"></a>
+	`;
 
 
-$(selector).click(function(){
-	$(selector).removeClass('active');
-	$(this).addClass('active');
-});
+
+// $(selector).click(function(){
+// 	$(selector).removeClass('active');
+// 	$(this).addClass('active');
+// });
